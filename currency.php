c18c6dded0c68a7522419961ed16c4fc6a2c7bca
joohnny3 <johnny31258@gmail.com>
2023-07-02 17:16:28 +0800

    完成

--- currency.php
+++ currency.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Currency</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300&family=Roboto&display=swap" rel="stylesheet">
   <link rel="icon" href="./image/bank.png" type="image/png">
   <link rel="stylesheet" href="spinkit.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
@@ -12,7 +15,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   <style>
     * {
-      font-family: "標楷體", arial, sans-serif;
+      font-family: 'Noto Sans TC','Roboto', sans-serif;
     }
 
     body {
