c08ee8846eead2f61940ff4cf068285b0b93280f
joohnny3 <johnny31258@gmail.com>
2023-07-02 17:22:05 +0800

    刪除不要的css

--- currency.php
+++ currency.php
@@ -8,7 +8,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300&family=Roboto&display=swap" rel="stylesheet">
-  <link rel="icon" href="./image/bank.png" type="image/png">
+  <link rel="icon" href="./image/money.png" type="image/png">
   <link rel="stylesheet" href="spinkit.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <script src="./confetti-js-master/dist/index.js"></script>
@@ -20,10 +20,6 @@
 
     body {
       background-color: #212529;
-    }
-
-    .container-fluid>svg {
-      margin-right: 3rem;
     }
 
     nav {
@@ -45,6 +41,7 @@
 
     .sk-grid {
       background-color: #ffdd07d6;
+      margin-right: 3rem;
     }
   </style>
 </head>
