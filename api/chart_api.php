d943715e1431e9ee276d4202eb1203e8dad60794
joohnny3 <johnny31258@gmail.com>
2023-08-12 03:46:44 +0800

    chore:新增database檔案

--- api/chart_api.php
+++ api/chart_api.php
@@ -1,26 +1,22 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
 $dsn = "mysql:host=localhost;charset=utf8;dbname=exchange_rate";
 $pdo = new PDO($dsn, 'root', '');
-
 $sql = "SELECT * FROM `exchange_rates`";
 $sql_data = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-// dd($chart_date);
 $chart_data = [];
 
 foreach ($sql_data as $key => $value) {
     $chart_data[$value['currency_id']][] = $value;
-    // dd($value);
 }
 
 echo json_encode($chart_data);
 
-dd($chart_data);
 
-function dd($array)
+/* function dd($array)
 {
     print "<pre>";
     print_r($array);
     print "</pre>";
-};
+}; */
